0d572f73b409d0e4c314c6090df0284caf256445
Melkoran <41878943+Melkoran@users.noreply.github.com>
2018-07-30 11:41:01 +0200

    Delete createfullmatrix.c

--- createfullmatrix.c
+++
@@ -1,8 +0,0 @@
-//createfullmatrix
-#include "lanczos.h"
-
-void createfullmatrix(FullMatrix *V, int m, int n) {
-     V->value = (double*)malloc(m*n*sizeof(double));   
-     V->nrows = m;
-     V->ncols = n;     
-}
